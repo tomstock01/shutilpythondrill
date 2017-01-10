38830c427cd3ad6526c10e126ad0225d1b567ff9
tomstock01 <tstock2@gmail.com>
2017-01-10 10:59:13 -0800

    added files via upload
    move files from folder A to folder B and print file paths

---
+++ pythondrill2_main.py
@@ -0,0 +1,24 @@
+# Python version 2.7.12
+# Author: Tom Stock
+# Shutil drill
+
+import shutil
+
+
+
+shutil.move('C:/Users/thoma/Desktop/FolderA/doc1.txt',
+            'C:/Users/thoma/Desktop/FolderB')
+
+shutil.move('C:/Users/thoma/Desktop/FolderA/doc2.txt',
+            'C:/Users/thoma/Desktop/FolderB')
+
+shutil.move('C:/Users/thoma/Desktop/FolderA/doc3.txt',
+            'C:/Users/thoma/Desktop/FolderB')
+
+shutil.move('C:/Users/thoma/Desktop/FolderA/doc4.txt',
+            'C:/Users/thoma/Desktop/FolderB')
+
+print 'C:/Users/thoma/Desktop/FolderA/doc1.txt'
+print 'C:/Users/thoma/Desktop/FolderA/doc2.txt'
+print 'C:/Users/thoma/Desktop/FolderA/doc3.txt'
+print 'C:/Users/thoma/Desktop/FolderA/doc4.txt'
