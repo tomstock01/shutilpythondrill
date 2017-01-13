16e6336b160e84036dbda5d4eca1bdf45778264e
tomstock01 <tstock2@gmail.com>
2017-01-13 10:45:33 -0800

    cleaned up for loop

--- pythondrill2_main.py
+++ pythondrill2_main.py
@@ -7,9 +7,14 @@ import os
 
 array1 = os.listdir(r'C:\Users\thoma\Desktop\FolderA')
 
-for i in range(len(array1)):
-    things = array1[i]
-    stuff = 'C:\\Users\\thoma\\Desktop\\FolderA\\' + things
-    shutil.move(stuff,'C:/Users/thoma/Desktop/FolderB')
-    print array1[i]
+##for i in range(len(array1)):
+##    #things = array1[i]
+##    stuff = 'C:\\Users\\thoma\\Desktop\\FolderA\\' + array1[i]
+##    shutil.move(stuff,'C:/Users/thoma/Desktop/FolderB')
+##    print stuff
 
+for i in array1:
+    #things = array1[i]
+    stuff = 'C:\\Users\\thoma\\Desktop\\FolderA\\' + i
+    shutil.move(stuff,'C:/Users/thoma/Desktop/FolderB')
+    print stuff
